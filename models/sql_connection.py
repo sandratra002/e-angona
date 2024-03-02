fbbe8fa9f933fc1d1e36bc2a727ac83049eee8cd
sandratra002 <ainarafanomazantsoa@gmail.com>
2024-03-02 22:32:23 +0300

    Creating sqlconnection class

---
+++ models/sql_connection.py
@@ -0,0 +1,10 @@
+import pyodbc
+import json
+
+class SQLConnection:
+    @staticmethod
+    def get_connection():
+        db_conf = json.load("../conf/database.json")
+        connectionString = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER=Sandratra;DATABASE=e-angona;UID=Aina;PWD=aina'
+        conn = pyodbc.connect(connectionString)
+        return conn
