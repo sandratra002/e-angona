fb857f82fc1398275e924487adb7dba2967d2943
sandratra002 <ainarafanomazantsoa@gmail.com>
2024-03-02 11:50:47 +0300

    Creating church_group class with its propreties

---
+++ models/church_group.py
@@ -0,0 +1,24 @@
+class ChurchGroup :
+    def __init__(self, id = None, name = None) -> None:
+        self._id = id
+        self._name = name
+        pass
+    
+    # class method
+    
+    # propreties definition
+    @property
+    def id (self):
+        return self._id
+    
+    @id.setter
+    def id (self, value):
+        self.id = value
+    
+    @property
+    def name (self):
+        return self._name
+    
+    @name.setter
+    def name (self, value):
+        self.name = value
